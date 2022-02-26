1471df62e5ff6f4b85ae4a22ace1294bdd049beb
navyapp <79969396+navyapp@users.noreply.github.com>
2022-02-26 23:21:20 +0530

    mainactivity

---
+++ CO3/Array_Adapter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listview=new ListView(this);
+        String[] sports={"Soccer","Cricket","Hockey","Baseball","Rugby"};
+        ArrayAdapter<String> sportsAdapter=new ArrayAdapter<String>(MainActivity.this,
+                R.layout.activity_main,R.id.textView,sports);
+        setContentView(listview);
+        listview.setAdapter(sportsAdapter);
+
+    }
+}
