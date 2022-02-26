fe88e006fa540535f26b1a173b1aab6272bb121d
navyapp <79969396+navyapp@users.noreply.github.com>
2022-02-26 23:00:12 +0530

    mainactivity

--- CO1/Calculator/MainActivity.java
+++ CO1/Calculator/MainActivity.java
@@ -1,1 +1,55 @@
+package com.example.calculator;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText ed1;
+    private EditText ed2;
+    private TextView tv1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ed1=(EditText)findViewById(R.id.ed1);
+        ed2=(EditText)findViewById(R.id.ed2);
+        tv1=(TextView) findViewById(R.id.tv1);
+    }
+
+
+    public void btnsum(View view)
+    {
+        int n1=Integer.parseInt(ed1.getText().toString());
+        int n2=Integer.parseInt(ed2.getText().toString()) ;
+        int result=n1+n2;
+        tv1.setText(String.valueOf(result));
+    }
+
+    public void btnsub(View view) {
+        int n1=Integer.parseInt(ed1.getText().toString());
+        int n2=Integer.parseInt(ed2.getText().toString()) ;
+        int result=n1-n2;
+        tv1.setText(String.valueOf(result));
+    }
+
+    public void btnmul(View view) {
+        int n1=Integer.parseInt(ed1.getText().toString());
+        int n2=Integer.parseInt(ed2.getText().toString()) ;
+        int result=n1*n2;
+        tv1.setText(String.valueOf(result));
+    }
+
+    public void btndiv(View view) {
+        int n1=Integer.parseInt(ed1.getText().toString());
+        int n2=Integer.parseInt(ed2.getText().toString()) ;
+        int result=n1/n2;
+        tv1.setText(String.valueOf(result));
+    }
+}
