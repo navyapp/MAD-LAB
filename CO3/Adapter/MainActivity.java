87f9cf9e70af79c22fe91641578d6bc53bff55c1
46_Navya_PP <79969396+navyapp@users.noreply.github.com>
2022-02-27 23:05:07 +0530

    mainactivity

--- CO3/Adapter/MainActivity.java
+++ CO3/Adapter/MainActivity.java
@@ -1,1 +1,27 @@
+package com.example.adapterandexception;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView lv=(ListView) findViewById(R.id.List);
+        String[] Colors={"Red","Green","Black","White","Yellow"};
+        ArrayAdapter<String> ColorAdapter=new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,Colors);
+        try{
+            lv.setAdapter(ColorAdapter);
+        }catch (Exception e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
