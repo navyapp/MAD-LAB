b3cc1d5ac5acab319030cb0a62a7d6bea78686be
46_Navya_PP <79969396+navyapp@users.noreply.github.com>
2022-02-27 18:20:52 +0530

    mainactivity

--- CO2/Toogle/MainActivity.java
+++ CO2/Toogle/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        togglebutton=(ToggleButton) findViewById(R.id.toggleButton);
+        togglebutton=(ToggleButton)findViewById(R.id.toggleButton);
         textview=(TextView)findViewById(R.id.textView);
     }
     public void onToggleClick(View view)
